1e1cbe731e5c1e954fae600f7428898ca594a039
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2023-10-25 10:55:43 +0200

    opdatering af  menu

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 // Husk at skrive til de andre før man pusher
 
-import java.util.ArrayList;
+import java.util.ArrayList;//----------------------------------------------------------------------------------------------------------------------
 import java.util.Scanner;
-//----------------------------------------------------------------------------------------------------------------------
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int indtastning = scanner.nextInt();
+
         System.out.println("Goddag, hvilken handling ønsker du?");
+        System.out.println();
         System.out.println("Tast 1 for oprettelse af aftale");
         System.out.println("Tast 2 for Ændring af aftale");
         System.out.println("Tast 3 for at slette aftale");
         System.out.println("Tast 4 for at se ledige tider");
         System.out.println("Tast 5 for at se tid og salgs historik");
 
+        Scanner scanner = new Scanner(System.in);
+        int indtastning = scanner.nextInt();
         switch(indtastning){
             case 1:
                 // Opret aftale
@@ -36,11 +38,16 @@ public class Main {
     }
 }
 //----------------------------------------------------------------------------------------------------------------------
-class aftale{
+class aftale extends kunde{
     String navn;
     double pris;
 }
 //----------------------------------------------------------------------------------------------------------------------
 class kalender{
     ArrayList<aftale> aftaler = new ArrayList <aftale>();
+}
+
+class kunde{
+    String navn;
+    double pris;
 }
