6a15741afc37a6f80e53049c592f961cc78f92ed
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2023-10-25 10:37:27 +0200

    opret menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
+// Husk at skrive til de andre før man pusher
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        int indtastning = scanner.nextInt();
 
+        switch(indtastning){
+            case 1:
+                // Opret aftale
+                break;
+            case 2:
+                // ændre aftale
+                break;
+            case 3:
+                // slet aftale
+                break;
+            case 4:
+                // Se kalender og ledige tider
+                break;
+            case 5:
+                // Se kalender over gamle tider
+                break;
+
+
+
+
+        }
     }
+}
+
+class aftale{
+    String navn;
+    double pris;
+}
+
+class kalender{
+    ArrayList<aftale> aftaler = new ArrayList <aftale>();
 }
